5133e3f4b7deef5a7fc733eae5f68b710f1ccf46
devnoom <archilmenabde@yahoo.com>
2024-04-07 17:09:02 +0400

    Initial Commit

---
+++ UIKITHomeWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKITHomeWork3
+//
+//  Created by MacBook Air on 07.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
